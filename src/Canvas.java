7d74f26d660833e05f8143c5c486809c8d4d2458
Krystle <krystleweinrich@yahoo.com>
2017-05-16 00:59:10 -0700

    added jar

--- src/Canvas.java
+++ src/Canvas.java
@@ -61,7 +61,7 @@ public class Canvas extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g); //To change body of generated methods, choose Tools | Templates
+        super.paintComponent(g);
         //draw all the shapes!
         list.forEach(shape ->{
             if (shape == selectedShape) {
