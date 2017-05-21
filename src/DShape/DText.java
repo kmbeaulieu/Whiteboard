f254e79e8dc48d703303cc366653f4cf72a328fb
Krystle <krystleweinrich@yahoo.com>
2017-05-20 17:47:46 -0700

    added disable textbox items for things not a text box

--- src/DShape/DText.java
+++ src/DShape/DText.java
@@ -8,32 +8,47 @@ public class DText extends DShape {
 
     public DText(DShapeModel dsm) {
         super(dsm);
-       
+
     }
 
     @Override
-	public void draw(Graphics g){
-		super.draw(g);
-                DTextModel dtm = (DTextModel)model;
-                g.setFont(dtm.getFont());
-                g.setColor(dtm.getColor());
-                g.drawString(dtm.getText(), dtm.getX(), dtm.getY());
-	}
-        
-        public void setText(String text){
-            DTextModel dtm = (DTextModel) model;
-            dtm.setText(text);
-        }
-        
-        public void setFont(Font f){
-            DTextModel dtm = (DTextModel) model;
-            dtm.setFont(f);
-        }
+    public void draw(Graphics g) {
+        super.draw(g);
+        DTextModel dtm = (DTextModel) model;
+        g.setFont(dtm.getFont());
+        g.setColor(dtm.getColor());
+        g.drawString(dtm.getText(), dtm.getX(), dtm.getY());
+    }
+
+    public void setText(String text) {
+        DTextModel dtm = (DTextModel) model;
+        dtm.setText(text);
+    }
+
+    public void setFont(Font f) {
+        DTextModel dtm = (DTextModel) model;
+        dtm.setFont(f);
+    }
+
+    public String getText() {
+        DTextModel dtm = (DTextModel) model;
+        return dtm.getText();
+    }
+
+    public Font getFont() {
+        DTextModel dtm = (DTextModel) model;
+        return dtm.getFont();
+    }
+    
+    public String getFontName(){
+        DTextModel dtm = (DTextModel) model;
+        return dtm.getFont().getName();
+    }
 
     @Override
     public String toString() {
         DTextModel dtm = (DTextModel) model;
-        return "DText{" + dtm.toString() +  '}';
+        return "DText{" + dtm.toString() + '}';
     }
-        
+
 }
